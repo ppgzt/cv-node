2d6360a8ffd676ecc1cf4935174e4b64b25eefcb
rasp-felpslima22 <rasp-felpslima22@raspberrypi>
2024-10-06 19:34:59 -0300

    Atualizacoes no bootstrap.sh para rodar no crontab

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
-export FLASK_APP=./app/index.py
-pipenv run flask --debug run -h 0.0.0.0
+#!/bin/bash
+dir="/$1"
+
+if [ "$#" -eq "0" ] 
+  then
+    dir=$(pwd)
+fi
+export FLASK_APP="$dir/app/index.py"
+
+source "$dir/venv/bin/activate"
+# exec "$@"
+
+flask --debug run -h 0.0.0.0
+
+# export FLASK_APP=./app/index.py
+# pipenv run flask --debug run -h 0.0.0.0
