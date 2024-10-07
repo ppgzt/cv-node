3bf52d6b79315eea863573ed9bf531307a93edf1
Ewerton Costa <ewertonsjp@gmail.com>
2024-10-06 21:09:49 -0300

    Add lock aos métodos de insert/update do BD

--- app/data/datasource/datasource.py
+++ app/data/datasource/datasource.py
@@ -1,6 +1,8 @@
 from tinydb import TinyDB, Query
 from app.domain.entities.basic import *
 
+from threading import Lock
+
 class Datasource(object):
 
     def __init__(self):
@@ -10,26 +12,38 @@ class Datasource(object):
         self.__run_table = 'runs'
         self.__itm_table = 'itens'
 
+        self.__lock = Lock()
+
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Datasource, cls).__new__(cls)
         return cls.instance
     
+    def __commit(self, f):
+        with self.__lock:
+            return f()
+    
     # Insert
     
     def insert_job(self, job: Job) -> int:
-        return self.db.table(self.__job_table).insert(job.to_dict())
+        return self.__commit(
+            lambda: self.db.table(self.__job_table).insert(job.to_dict())
+        )
 
     def insert_run(self, run: Run) -> int:
-        return self.db.table(self.__run_table).insert(run.to_dict())
+        return self.__commit(
+            lambda: self.db.table(self.__run_table).insert(run.to_dict()) 
+        )
 
     def update_run(self, run_id: int, run: Run):
-        self.db.table(self.__run_table).update(
-            run.to_dict(), doc_ids=[run_id]
+        self.__commit(
+            self.db.table(self.__run_table).update(run.to_dict(), doc_ids=[run_id])
         )
 
     def insert_item(self, item: RunItem) -> int:
-        return self.db.table(self.__itm_table).insert(item.to_dict())
+        return self.__commit(
+            self.db.table(self.__itm_table).insert(item.to_dict())
+        )
 
     # List
 
