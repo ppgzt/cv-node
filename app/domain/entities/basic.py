a036cf89bfd150041c2421762844a82523a225cc
Ewerton Costa <ewertonsjp@gmail.com>
2024-10-18 14:36:59 -0300

    Ajustes de formatação nas classes.

--- app/domain/entities/basic.py
+++ app/domain/entities/basic.py
@@ -3,35 +3,30 @@ from enum import Enum
 
 class Job:
 
-    def __init__(self, begin_at: datetime):
+    def __init__(self, begin_at: datetime, final_at: datetime = None):
         self.begin_at = begin_at
-        self.final_at = None
+        self.final_at = final_at
 
+    def to_tuple(self):
+        return (
+            self.begin_at.isoformat(), 
+            self.final_at.isoformat() if self.final_at is not None else None
+        )
+    
     def to_dict(self):
         return {
             'begin_at': self.begin_at.isoformat(),
             'final_at': self.final_at.isoformat() if self.final_at is not None else None
-        }    
-
-    def to_tuple(self):
-        return (self.begin_at.isoformat(), self.final_at.isoformat() if self.final_at is not None else None)
+        }
 
 class Run:
 
-    def __init__(self, begin_at: datetime, thing_id: str, job_id: int):
+    def __init__(self, begin_at: datetime, thing_id: str, job_id: int, final_at: datetime = None):
         self.begin_at = begin_at
-        self.final_at = None
+        self.final_at = final_at
         self.thing_id = thing_id
         self.job_id = job_id
 
-    def to_dict(self):
-        return {
-            'begin_at': self.begin_at.isoformat(),
-            'final_at': self.final_at.isoformat() if self.final_at is not None else None,
-            'thing_id': self.thing_id,
-            'job_id': self.job_id
-        }
-    
     def to_tuple(self):
         return (
             self.begin_at.isoformat(), 
@@ -39,6 +34,14 @@ class Run:
             self.thing_id,
             self.job_id
         )
+    
+    def to_dict(self):
+        return {
+            'begin_at': self.begin_at.isoformat(),
+            'final_at': self.final_at.isoformat() if self.final_at is not None else None,
+            'thing_id': self.thing_id,
+            'job_id': self.job_id
+        }
 
 class RunItemType(Enum):
     IMAGE = 0
@@ -57,6 +60,15 @@ class RunItem:
         self.file_path   = file_path
         self.run_id = run_id
 
+    def to_tuple(self):
+        return (
+            self.status.name, 
+            self.sensor, 
+            self.type.name, 
+            self.file_path, 
+            self.run_id
+        )
+    
     def to_dict(self):
         return {
             'status': self.status.name,
@@ -66,6 +78,3 @@ class RunItem:
             'run_id': self.run_id,
         }
     
-
-    def to_tuple(self):
-        return (self.status.name, self.sensor, self.type.name, self.file_path, self.run_id)
